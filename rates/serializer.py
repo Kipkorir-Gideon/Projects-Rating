f2041489a470bc86a5ef445273245913d18ac66f
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-11 20:15:12 +0300

    Add project class serializer.

--- rates/serializer.py
+++ rates/serializer.py
@@ -5,3 +5,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('id', 'bio', 'user', 'photo')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ('id', 'title', 'screenshot', 'description', 'day_posted', 'owner', 'url')
