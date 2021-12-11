317d7157cbb9101cd8dd61c034bc926ac97a572c
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-11 11:35:45 +0300

    Add a profile class and its properties. Also define its methods.

--- rates/models.py
+++ rates/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = CloudinaryField(blank=True, default = 'photo')
+
+    #Creates a profile when a user is created
+    @receiver(post_save, sender = User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    #Saves the User's profile information
+    @receiver(post_save, sender = User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_user_profile(self):
+        self.save()
+
+    def delete_user_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return "%s profile" % self.user
