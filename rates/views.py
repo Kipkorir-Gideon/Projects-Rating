d0dcdf1d5545508c4c0f06dff63af52990a01398
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-11 12:40:01 +0300

    Create a view function for posted projects as homepage and add its url.

--- rates/views.py
+++ rates/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def projects(request):
+    projects = Projects.objects.all()
+    return render(request, 'projects.html', {'projects': projects})
+    
