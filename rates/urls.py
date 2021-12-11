1a56bf4aa60d7a472bedad6925d54319332537fc
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-11 14:35:12 +0300

    Add a url path for search.

--- rates/urls.py
+++ rates/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path,re_path
 from . import views
+from . import views as app_views
 
 urlpatterns=[
     path('',views.projects,name='projects'),
+    re_path(r'^search/$',app_views.search_projects,name='search'),
+
 ]
 if settings.DEBUG:
 
