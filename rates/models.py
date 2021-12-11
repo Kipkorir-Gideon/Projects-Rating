8e92fc067368b58733c25f665a97d958321cecce
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-11 12:00:22 +0300

    Add projects model methods.

--- rates/models.py
+++ rates/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 
 # Create your models here.
 class Profile(models.Model):
@@ -39,3 +41,37 @@ class Projects(models.Model):
     day_posted = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     url = models.URLField()
+
+    def save_user_project(self):
+        self.save()
+
+    def delete_user_project(self):
+        self.delete()
+
+    @classmethod
+    def get_user_project(cls):
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def search_user_project(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
+
+    @classmethod
+    def get_project_by_owner(cls, owner):
+        projects = cls.objects.filter(owner=owner)
+        return projects
+
+    @classmethod
+    def get_project_by_id(cls, project_id):
+        try:
+            project = Projects.objects.get(project_id=project_id)
+
+        except ObjectDoesNotExist:
+            raise Http404()
+
+        return project
+
+    def __str__(self):
+        return "%s projects" % self.title
