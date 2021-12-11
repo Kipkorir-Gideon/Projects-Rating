1c332cb1ad0fb444de5f16567db4bc39a7acc3d9
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-11 14:30:08 +0300

    Add a search function for searching projects.

--- rates/views.py
+++ rates/views.py
@@ -5,4 +5,18 @@ from .models import *
 def projects(request):
     projects = Projects.objects.all()
     return render(request, 'projects.html', {'projects': projects})
-    
+
+
+
+# search projects 
+def search_projects(request):
+    if 'search_project' in request.GET and request.GET["search_project"]:
+        search_term = request.GET.get("keyword")
+        searched_projects = Projects.search_user_project(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
