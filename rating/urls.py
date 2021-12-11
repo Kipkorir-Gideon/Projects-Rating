b16b72ed7ed57f838f5619b5fc15343c8a0ed17f
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-11 12:11:33 +0300

    Innclude urls paths on projects urls patterns.

--- rating/urls.py
+++ rating/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views 
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('rates.urls')),
+    path('accounts/',include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(next_page='/')),
+    path('api-token-auth/', obtain_auth_token),
 ]
