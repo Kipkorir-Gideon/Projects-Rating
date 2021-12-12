d372e3967ef3556e68a5eb81c2ab0dbd50efd1a9
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-12 21:20:49 +0300

    Add a profile form class.

---
+++ rates/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import *
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('photo', 'user', 'bio')
